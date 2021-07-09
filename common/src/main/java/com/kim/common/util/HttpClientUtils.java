a329f02f0d387d3dd4d86fc0f7eda2ff9eeff599
kim <278362446@qq.com>
2021-07-09 17:55:07 +0800

    httpclient

--- common/src/main/java/com/kim/common/util/HttpClientUtils.java
+++ common/src/main/java/com/kim/common/util/HttpClientUtils.java
@@ -16,7 +16,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
