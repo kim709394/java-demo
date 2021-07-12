62b89b8a2df7e96e800b3121cabd6e66085e87fb
kim <278362446@qq.com>
2021-07-12 18:13:12 +0800

    httpclient连接池

--- common/src/main/java/com/kim/common/util/HttpClientPoolUtils.java
+++ common/src/main/java/com/kim/common/util/HttpClientPoolUtils.java
@@ -4,17 +4,27 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
@@ -22,16 +32,69 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
  * @Author kim
- * httpclient工具类
- * @Since 2021/7/9
+ * httpclient连接池工具类
+ * @Since 2021/7/12
  */
-public class HttpClientUtils {
+public class HttpClientPoolUtils {
+
+    //请求客户端
+    private static CloseableHttpClient httpClient;
+
+    //初始化httpclient连接池
+    static {
+        try {
+            //设置https证书支持
+            SSLContextBuilder builder = new SSLContextBuilder();
+            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
+            // 配置同时支持 HTTP 和 HTPPS
+            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
+                    .<ConnectionSocketFactory>create()
+                    .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                    .register("https", sslsf)
+                    .build();
+            // 初始化连接管理器
+            PoolingHttpClientConnectionManager poolConnManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+            poolConnManager.setMaxTotal(640);// 同时最大连接数
+            // 设置最大路由,同一url连接对象的最大并发数量
+            poolConnManager.setDefaultMaxPerRoute(320);
+            /**
+             * MaxtTotal和DefaultMaxPerRoute的区别：
+             * MaxtTotal指整个池子的所有连接数的最大值，DefaultMaxPerRoute是指同一个url请求的最大并发数
+             * 比如一个池子里MaxtTotal设置为20，DefaultMaxPerRoute设置为10
+             * 先执行100次url为a的请求，后执行100次url为b的请求，那么连接池里只有10个a连接，10个b连接，总数是20个。
+             * 如果继续执行url为c的请求，那么池子里的a和b连接将会分一部分作为c的连接
+             */
+            //封装池化的httpclient
+            RequestConfig config = RequestConfig
+                    .custom()
+                    .setConnectTimeout(5000)
+                    .setConnectionRequestTimeout(5000)
+                    .setSocketTimeout(5000)
+                    .build();
+            httpClient = HttpClients.custom()
+                    .setConnectionManager(poolConnManager)
+                    .setDefaultRequestConfig(config)
+                    // 设置重试次数
+                    .setRetryHandler(new DefaultHttpRequestRetryHandler(2, false)).build();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     /**
      * get请求，参数拼接在url后面,不设置headers
@@ -48,8 +111,8 @@ public class HttpClientUtils {
      * @param headers Map<String,String> 请求头
      * @return String 接口返回值
      * */
-    public static String doGet(String url,Map<String,String> headers) {
-        return doGet(null,url,headers);
+    public static String doGet(String url, Map<String,String> headers) {
+        return doGet(httpClient,url,headers);
     }
 
     /**
@@ -70,7 +133,7 @@ public class HttpClientUtils {
      * @return String 接口返回值
      * */
     public static String doGetWithParam(String url, Map<String, String> param,Map<String,String> headers){
-        return doGetWithParam(null,url,param,headers);
+        return doGetWithParam(httpClient,url,param,headers);
     }
 
     /**
@@ -91,7 +154,7 @@ public class HttpClientUtils {
      * @return String 接口返回值
      * */
     public static String doPost(String url, Map<String, String> param,Map<String,String> headers){
-        return doPost(null,url,param,headers);
+        return doPost(httpClient,url,param,headers);
     }
 
     /**
@@ -112,7 +175,7 @@ public class HttpClientUtils {
      * @return String 接口返回值
      * */
     public static String doPostByJson(String url, String jsonString,Map<String,String> headers){
-        return doPostByJson(null,url,jsonString,headers);
+        return doPostByJson(httpClient,url,jsonString,headers);
     }
 
     /**
@@ -121,7 +184,7 @@ public class HttpClientUtils {
      * @param in    文件流
      * @return 返回值
      * */
-    public static String httpClientUploadFile(String url,InputStream in){
+    public static String httpClientUploadFile(String url, InputStream in){
         return httpClientUploadFile(url,in,null);
     }
 
@@ -133,7 +196,7 @@ public class HttpClientUtils {
      * @return 返回值
      * */
     public static String httpClientUploadFile(String url, InputStream in,Map<String,String> param){
-        return httpClientUploadFile(null,url,in,param);
+        return httpClientUploadFile(httpClient,url,in,param);
     }
 
     private static String doGet(CloseableHttpClient httpClient,String url,Map<String,String> headers) {
@@ -159,7 +222,6 @@ public class HttpClientUtils {
 
         } finally {
             try {
-                httpClient.close();
                 if (response != null) {
                     response.close();
                 }
@@ -201,7 +263,6 @@ public class HttpClientUtils {
             e.printStackTrace();
         } finally {
             try {
-                httpClient.close();
                 if (response != null) {
                     response.close();
                 }
@@ -243,7 +304,6 @@ public class HttpClientUtils {
             e.printStackTrace();
         } finally {
             try {
-                httpClient.close();
                 if (response != null) {
                     response.close();
                 }
@@ -281,7 +341,6 @@ public class HttpClientUtils {
             e.printStackTrace();
         } finally {
             try {
-                httpClient.close();
                 if (response != null) {
                     response.close();
                 }
@@ -298,6 +357,7 @@ public class HttpClientUtils {
             httpClient = HttpClients.createDefault();
         }
         String result = "";
+        HttpResponse response=null;
         try {
             HttpPost httpPost = new HttpPost(url);
             MultipartEntityBuilder builder = MultipartEntityBuilder.create();
@@ -307,7 +367,7 @@ public class HttpClientUtils {
             }
             HttpEntity entity = builder.build();
             httpPost.setEntity(entity);
-            HttpResponse response = httpClient.execute(httpPost);// 执行提交
+            response = httpClient.execute(httpPost);// 执行提交
             HttpEntity responseEntity = response.getEntity();
             if (responseEntity != null) {
                 // 将响应内容转换为字符串
@@ -317,16 +377,9 @@ public class HttpClientUtils {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            try {
-                httpClient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
         return result;
     }
-
 
 
 }
