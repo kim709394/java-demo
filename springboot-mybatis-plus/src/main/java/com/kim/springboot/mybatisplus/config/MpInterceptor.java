36df13b30e177e15516d16b233c07758ce5da651
kim <278362446@qq.com>
2021-11-25 14:19:59 +0800

    springboot-mybatis-plus-demo

--- springboot-mybatis-plus/src/main/java/com/kim/springboot/mybatisplus/config/MpInterceptor.java
+++ springboot-mybatis-plus/src/main/java/com/kim/springboot/mybatisplus/config/MpInterceptor.java
@@ -1,9 +1,11 @@
 package com.kim.springboot.mybatisplus.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.IllegalSQLInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import net.sf.jsqlparser.expression.Expression;
@@ -22,14 +24,34 @@ public class MpInterceptor {
     //插件配置
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        /**
+         * 使用多个功能需要注意顺序关系,建议使用如下顺序
+         *
+         * 多租户,动态表名
+         * 分页,乐观锁
+         * sql性能规范,防止全表更新与删除
+         * 总结: 对sql进行单次改造的优先放入,不对sql进行改造的最后放入
+         * */
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //多租户插件，注意要配置在分页插件之前，否则分页数据记录统计不会加入租户字段过滤
+        interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new MyTenantLine()));
         //分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //sql性能规范插件
+        interceptor.addInnerInterceptor(new IllegalSQLInnerInterceptor());
         //阻止全表更新和删除插件
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
-        //多租户插件
-        interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new MyTenantLine()));
+
         return interceptor;
+    }
+
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
 
     //行级租户处理器
