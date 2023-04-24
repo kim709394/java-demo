19fceb9d1b8a0d4ee9e49d78ec9837458f06c0d3
kim <278362446@qq.com>
2023-04-24 13:46:02 +0800

    动态修改logback的日志存储路径

--- spring-boot/src/test/java/com/kim/spring/boot/test/SpringbootDemoTest.java
+++ spring-boot/src/test/java/com/kim/spring/boot/test/SpringbootDemoTest.java
@@ -2,11 +2,21 @@ package com.kim.spring.boot.test;
 
 import com.kim.spring.boot.SpringbootDemoApplication;
 import com.kim.spring.boot.config.MyConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileOutputStream;
+import java.util.List;
 
 /**
  * @author huangjie
@@ -15,6 +25,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootDemoApplication.class)
+@Slf4j
 public class SpringbootDemoTest {
 
     @Autowired
@@ -26,5 +37,34 @@ public class SpringbootDemoTest {
         System.out.println(configuration);
     }
 
+    @Test
+    @DisplayName("动态修改logback的日志存储路径")
+    public void changeLogFile() throws Exception{
+
+
+
+        for(int i=0; i<10 ; i++){
+            log.info("日志打印在哪里？");
+            Thread.sleep(10*1000L);
+            SAXReader logXml=new SAXReader();
+            String path = this.getClass().getClassLoader().getResource("log/logback-dev.xml").getPath();
+            System.out.println(path);
+            Document document = logXml.read(this.getClass().getClassLoader().getResourceAsStream("log/logback-dev.xml"));
+            Element property = document.getRootElement().element("property");
+            List<Attribute> attributes = property.attributes();
+            int finalI = i;
+            attributes.stream().forEach(attribute -> {
+                if(attribute.getName().equals("value")){
+                    attribute.setValue("C:/log/foo"+ finalI);
+                }
+            });
+            XMLWriter writer=new XMLWriter(new FileOutputStream(path));
+            writer.write(document);
+            writer.close();
+        }
+
+
+
+    }
 
 }
